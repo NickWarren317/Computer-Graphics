4f30960c614f6d94e85f68613c02341c45e410a1
NickWarren317 <yoitsbiggiecheese@gmail.com>
2024-09-11 17:19:22 -0500

    works a bit now

--- Projects/tetris.js
+++ Projects/tetris.js
@@ -9,10 +9,19 @@
 
 "use strict";
 
+//event variables
+var is_mousedown = false;
+var shift_key = false;
+var t1, t2;
+
+
 //main variables, this is where the cookin happens.
 var gl;
-var points=[];
-var sqaure_size = 0.025;
+let points=[];
+var colors=[];
+var square_size = 0.025;
+var offset = 2.03;
+var death_line = -0.7;
 
 //holds the individual squares...
 var num_shapes = 0;
@@ -25,6 +34,7 @@ var square_centers = [];
 var num_minos = 0;
 var pieces_index = [0];
 var minos = [];
+var mino_colors = [];
 
 //rotation varaible of a tetrimino, may or may not be used...
 var pieces_orientation = [];
@@ -99,6 +109,18 @@ var long_block = [
         add_left,
         add_right,
         add_outer_right
+    ],
+    [
+        make_square_at_point,
+        add_up,
+        add_down,
+        add_outer_bottom
+    ],
+    [
+        make_square_at_point,
+        add_left,
+        add_right,
+        add_outer_left
     ]
 ]; 
 
@@ -114,15 +136,39 @@ var right_step_block = [
         add_up,
         add_right,
         add_bottom_right
+    ],
+    [
+        make_square_at_point,
+        add_up,
+        add_left,
+        add_top_right
+    ],
+    [
+        make_square_at_point,
+        add_up,
+        add_left,
+        add_bottom_left
     ]
 ];
 
 var left_step_block = [
     [
         make_square_at_point,
-        add_up,
+        add_left,
+        add_down,
+        add_bottom_right
+    ],
+    [
+        make_square_at_point,
+        add_down,
         add_right,
-        add_top_left
+        add_top_right
+    ],
+    [
+        make_square_at_point,
+        add_left,
+        add_down,
+        add_bottom_right
     ],
     [
         make_square_at_point,
@@ -157,7 +203,7 @@ var clockwise_l_block = [
         add_right,
         add_left,
         add_top_right
-    ],
+    ]
 ];
 
 var counter_clockwise_l_block = [
@@ -210,9 +256,6 @@ var bottom_line = [
 window.onload = function init()
 {
     var canvas = document.getElementById( "gl-canvas" );
-    gl = canvas.getContext('webgl2');
-    if (!gl) { alert( "WebGL 2.0 isn't available" ); }
-
     //
     //  Initialize our data for a single triangle
     //
@@ -221,46 +264,110 @@ window.onload = function init()
     //begin gathering the point
     //  Configure WebGL
     //
-    gl.viewport(0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-
-    //  Load shaders and initialize attribute buffers
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
-
     //
-    var line = -0.65;
-    for(var i = 0; i < tetrimino_templates.length; i++){
-        draw_tetrimino(tetrimino_templates[i], line + (i * 0.2), 0.825, 0.025, 0);
-    } 
-
-    rotate_clockwise(0);
     // Load the data into the GPU
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId );
-
-
-    //draw top and bottom border lines
-
-    var displacement = 0.7;
-   // points.push(vec2(-1,displacement),vec2(1,displacement));
-    //points.push(vec2(-1,-displacement),vec2(1,-displacement));
-
-
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
-
-    // Associate out shader variables with our data buffer
-
-    var aPosition = gl.getAttribLocation( program, "aPosition" );
-    gl.vertexAttribPointer( aPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( aPosition );
-
+    
     console.log(points);
+
     render();
+
+    canvas.addEventListener("mouseup", function(event){
+        is_mousedown = false;
+      });
+
+    canvas.addEventListener("mousedown", function(event){
+        is_mousedown = true;
+        var x = event.pageX - canvas.offsetLeft;
+        var y = event.pageY - canvas.offsetTop;
+        t1 = vec2(2*x/canvas.width-1, 
+                    2*(canvas.height-y)/canvas.height-1);
+        
+        console.log(t1);
+        
+        
+        
+        if(shift_key){
+            var c_object = is_within_mino(t1[0], t1[1]);
+            if(c_object != -1){
+                rotate_counter_clockwise(c_object, t1[0], t2[1]);
+            }
+        }
+      });
+
+    canvas.addEventListener("mousemove", function(event){
+        if (is_mousedown) {
+          var x = event.pageX - canvas.offsetLeft;
+          var y = event.pageY - canvas.offsetTop;
+
+          t2 = vec2(2*x/canvas.width-1, 2*(canvas.height-y)/canvas.height-1);
+          
+          var c_object = is_within_mino(t1[0], t1[1]);
+
+          if(c_object != -1){
+            move_mino_x(c_object, t2[0] - t1[0]);
+            move_mino_y(c_object, t2[1] - t1[1]);
+            redraw_minos();
+          }
+
+          t1 = t2;
+        }
+    } );
+
+    document.addEventListener("keydown", function(event){
+        if(event.key === 'Shift'){
+            shift_key = true;
+        }
+    });
+    document.addEventListener("keyup", function(event){
+        if(event.key === 'Shift'){
+            shift_key = false;
+        }
+    });
+
+
 };
 
+function move_mino_x(mino_idx, dist){
+    if(minos[mino_idx] === -1) return;  
+    square_centers[mino_idx][0] = square_centers[mino_idx][0] + dist;
+    redraw_minos();
+}
 
-var offset = 2.03;
+function move_mino_y(mino_idx, dist){
+    if(minos[mino_idx] === -1) return; 
+    square_centers[mino_idx][1] = square_centers[mino_idx][1] + dist;
+    
+    redraw_minos();
+}
+
+function is_within_mino(x, y){
+    //iterate each mino, checking to see if the point is within the boundaries of a square.
+    for(var i = 0; i < minos.length; i++){
+        var p_idx = pieces_index[i];
+        for (var j = p_idx; j < p_idx + 16; j = j + 4){
+            //get the 4 points
+            var p1 = points[j];   //t_right
+            var p2 = points[j+1]; //t_left
+            var p3 = points[j+2]; //b_left
+            var p4 = points[j+3]; //b_right
+
+
+            //check if y is less than top y and greater than bottom y
+            if(y < p1[1] && y > p3[1]){
+
+                //check if x is less than right x and greater than left x
+                if(x < p4[0] && x > p2[0]){
+                    //once found, return the index of the mino
+                    return i;
+                }
+            }
+        }
+    }
+    //-1 if not in a mino
+    return -1;
+    
+}
+
 
 //rotates points between the two indicies of the points
 //(−(y−b)+a,(x−a)+b)
@@ -268,21 +375,37 @@ function rotate_clockwise(mino_idx){
     //Iterate the template....
     //needs a way to keep track of what type of mino each is....
     //and what is the current orientation
-    var center = square_centers[mino_idx];
-
-    var template = tetrimino_templates[mino_idx];
     var orient = pieces_orientation[mino_idx];
-
-    var x = pieces_index[mino_idx];
-
-    points.splice(x, x + 16);
     
-    draw_tetrimino(template, center[0], center[1], 0.025, (orient + 1) % template.length)
+    draw_tetrimino(minos[mino_idx], square_centers[mino_idx][0], square_centers[mino_idx][1], 0.025, (orient + 1) % minos[mino_idx].length);
+
+    minos[mino_idx] = -1;
+    pieces_orientation[mino_idx] = orient + 1
+
+    redraw_minos();
 
 }
 
-function rotate_counter_clockwise(start, end){
-    //Decrement the template
+function rotate_counter_clockwise(mino_idx, x , y){
+    //Iterate the template....
+    //needs a way to keep track of what type of mino each is....
+    //and what is the current orientation
+    var orient = pieces_orientation[mino_idx];
+    
+    console.log(points);
+    console.log(points.length);
+    console.log(minos);
+    
+    
+    draw_tetrimino(minos[mino_idx], square_centers[mino_idx][0], square_centers[mino_idx][1], 0.025, (orient + 3) % minos[mino_idx].length);
+
+    //move tetrimino to give the illusion that it is rotating about the center of the clicked mino
+
+
+    minos[mino_idx] = -1;
+    pieces_orientation[mino_idx] = orient + 1
+
+    redraw_minos();
 }
 
 function add_up(x,y,size){
@@ -323,20 +446,64 @@ function add_outer_bottom(x,y,size){
 }
 
 
-function draw_tetrimino(instructions, x, y, size, orient = 0){
-    pieces_index[num_minos] = num_minos * 16;
+function redraw_minos(){
 
+    //clear shape buffers...
+    var temp_points = points;
+    var temp_centers = square_centers;
+    var temp_minos = minos;
+
+    //clear things
+    minos=[];
+    start_idx = [];
+    num_minos = 0;
+    points = [];
+    num_shapes = 0;
+    square_centers = []
+
+    //redraw valid minos
+    for(var i = 0; i < temp_minos.length; i++){
+        //minos marked as -1 will be ignored, as they are deleted or rotated...
+        if(temp_minos[i] === -1 || is_mino_under_line(i, temp_points)){
+        //minos are redrawn when still there
+        } else {
+            draw_tetrimino(temp_minos[i], temp_centers[i][0], temp_centers[i][1], square_size, pieces_orientation[i]);
+        }
+    }
+
+    render();
+}
+
+function is_mino_under_line(mino_idx, p){
+    var start = pieces_index[mino_idx];
+    for (var i = start; i < start + 16; i++){
+        var y = p[i][1];
+        if(y <= death_line) {
+            minos[mino_idx] = -1;
+            return true;
+        }
+    }
+    return false;
+}
+
+function draw_tetrimino(instructions, x, y, size,orient = 0){
+    pieces_index[num_minos] = num_minos * 16;
     minos[num_minos] = instructions;
     pieces_orientation[num_minos] = orient;
+    square_centers[num_minos] = vec2(x,y);
+    //mino_colors[num_minos] = color;
 
     for(var i = 0 ; i < instructions[orient].length ; i++){
         instructions[orient][i](x,y,size);
     }
 
+    num_minos = num_minos + 1;                                  
+}                 
 
-    num_minos = num_minos + 1;
+function color_mino(color, mino_idx){
+
+    
 }
-
 
 function make_square_at_point(x, y, size){
     
@@ -350,8 +517,6 @@ function make_square_at_point(x, y, size){
         start_idx[num_shapes] = start_idx[num_shapes - 1] + num_positions[num_shapes - 1];
     }
 
-    square_centers[num_shapes] = vec2(x,y);
-
     num_shapes = num_shapes + 1;
 
 
@@ -362,22 +527,43 @@ function make_square_at_point(x, y, size){
     
 }
 
-
-function get_distance(x1,y1,x2,y2){
-    return Math.sqrt((x1 - x2)**2 +(y1 - y2)**2);   
-}
-
-
 function render() {
+    var canvas = document.getElementById("gl-canvas");
+
+    gl = canvas.getContext('webgl2');
+    if (!gl) { alert( "WebGL 2.0 isn't available" ); }
+
+    gl.viewport(0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+    //  Load shaders and initialize attribute buffers
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+
+    //draw top minos
+    var line = -0.65;
+    for(var i = 0; i < tetrimino_templates.length; i++){
+        draw_tetrimino(tetrimino_templates[i], line + (i * 0.2), 0.825, 0.025, 0);
+    } 
+
+
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+
+    // Associate out shader variables with our data buffer
+    var aPosition = gl.getAttribLocation( program, "aPosition" );
+    gl.vertexAttribPointer( aPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( aPosition );
+
+
+
     gl.clear( gl.COLOR_BUFFER_BIT );
 
-    for(var i=0; i<num_shapes; i++) {
-        gl.drawArrays( gl.TRIANGLE_FAN, start_idx[i], num_positions[i] );
+    for(var i=0; i < num_shapes; i++) {
+        gl.drawArrays(gl.TRIANGLE_FAN, start_idx[i], num_positions[i] );
     }
-
-    //draw lines
-    var boundry_offset = start_idx[num_shapes-1] + num_positions[num_shapes-1];
-    //gl.drawArrays(gl.LINE_STRIP, boundry_offset, 2);
-    //gl.drawArrays(gl.LINE_STRIP, boundry_offset + 2, 2);
+    //draw top and bottom border lines
 
 }
